050ea6c6277783bf915bc434586994bdb7619e8c
marcos.almeida <marcos.almeida@neogrid.com>
2021-02-28 15:51:20 -0300

    nav bar - login

--- reddit-web/src/components/NavBar.tsx
+++ reddit-web/src/components/NavBar.tsx
@@ -1,13 +1,19 @@
 import React from "react";
-import { Box, Flex, Link } from "@chakra-ui/react";
+import { Box, Button, Flex, Link } from "@chakra-ui/react";
 import NextLink from "next/link";
+import { useMeQuery } from "../generated/graphql";
 
 interface NavBarProps {}
 
 export const NavBar: React.FC<NavBarProps> = ({}) => {
-  return (
-    <Flex bg="tomato" p={4}>
-      <Box ml={"auto"}>
+  const [{ data, fetching }] = useMeQuery();
+  let body = null;
+
+  if (fetching) {
+    // data is loading
+  } else if (!data?.me) {
+    body = (
+      <>
         <NextLink href="/login">
           <Link color="white" mr={2}>
             Login
@@ -16,7 +22,20 @@ export const NavBar: React.FC<NavBarProps> = ({}) => {
         <NextLink href="/register">
           <Link color="white">Register</Link>
         </NextLink>
-      </Box>
+      </>
+    );
+  } else {
+    body = (
+      <Flex>
+        <Box mr={2}>{data.me.username}</Box>
+        <Button variant="link">Logout</Button>
+      </Flex>
+    );
+  }
+
+  return (
+    <Flex bg="tomato" p={4}>
+      <Box ml={"auto"}>{body}</Box>
     </Flex>
   );
 };
