cb59f36d57f6315512172ca54954eb1b5fcb0775
marcos.almeida <marcos.almeida@neogrid.com>
2021-02-28 00:13:04 -0300

    login page

---
+++ reddit-web/src/pages/login.tsx
@@ -0,0 +1,58 @@
+import { Button, Box } from "@chakra-ui/react";
+import { Form, Formik } from "formik";
+import React from "react";
+import { InputField } from "../components/InputField";
+import { useLoginMutation } from "../generated/graphql";
+import { toErrorMap } from "../util/toErrorMap";
+import { Wrapper } from "./Wrapper";
+import { useRouter } from "next/router";
+
+const Login: React.FC<{}> = ({}) => {
+  const router = useRouter();
+  const [, login] = useLoginMutation();
+  return (
+    <Wrapper variant="small">
+      <Formik
+        initialValues={{ username: "", password: "" }}
+        onSubmit={async (values, { setErrors }) => {
+          const response = await login({ options: values });
+          if (response.data?.login.errors) {
+            setErrors(toErrorMap(response.data.login.errors));
+          } else if (response.data?.login.user) {
+            router.push("/");
+          }
+        }}
+      >
+        {({ isSubmitting }) => (
+          <Form>
+            <Box mt={4}>
+              <InputField
+                name="username"
+                placeholder="username..."
+                label="UserName"
+              ></InputField>
+            </Box>
+            <Box mt={4}>
+              <InputField
+                name="password"
+                placeholder="password..."
+                label="Password"
+                type="password"
+              ></InputField>
+            </Box>
+            <Button
+              mt={4}
+              type="submit"
+              colorScheme="teal"
+              isLoading={isSubmitting}
+            >
+              Login
+            </Button>
+          </Form>
+        )}
+      </Formik>
+    </Wrapper>
+  );
+};
+
+export default Login;
